ce0244ea623cec1c411ddc9b085b7778a1c89ad4
Ron Shalevich <ronshalevich@Rons-MacBook-Air.local>
2017-07-27 15:44:00 -0700

    create a class to show movies details

---
+++ center.py
@@ -0,0 +1,14 @@
+import movies
+
+toy_story = movies.Movie("Toy Story",
+                         "A story of a boy and his toys",
+                         "https://goo.gl/images/h95syN",
+                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+print (toy_story.storyline)
+
+avatar = movies.Movie("Avatar",
+                      "A marine on an alien planet",
+                      "https://goo.gl/images/EJ7mBN",
+                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.storyline)
+avatar.show_trailer()
